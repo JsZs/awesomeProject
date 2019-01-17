16ef50b2379c92f2fba2d80b71e381accc2df841
cst5201 <1015485519@qq.com>
2019-01-17 23:40:54 +0800

    完善分布式模块，加入去重

--- src/DistributedCrawler/engine/concurrent.go
+++ src/DistributedCrawler/engine/concurrent.go
@@ -1,27 +1,46 @@
 package engine
 
-import (
-	"log"
-)
+type ConcurrentEngine struct {
+	Scheduler        Scheduler
+	WorkerCount      int
+	ItemChan         chan Item //ItemSaver
+	RequestProcessor Processor //RPC会调用
+}
 
-type ConcurrentEngine struct{
-	Scheduler Scheduler
-	WorkerCount int
+type Processor func(request Request) (ParseResult, error)
+
+var visitedUrl = make(map[string]bool) //map判断url是否重复
+
+func isDuplicate(url string) bool {
+	if visitedUrl[url] {
+		return true
+	}
+	visitedUrl[url] = true
+	return false
+}
+
+type ReadyNotifier interface {
+	WorkerReady(chan Request)
 }
 type Scheduler interface {
 	Submit(Request)
-	ConfigureMasterWorkerChan(chan Request)
-	WorkerReady(chan Request)
+	//ConfigureMasterWorkerChan(chan Request)
+	//WorkerReady(chan Request)
+	WorkChan() chan Request
 	Run()
+	ReadyNotifier
 }
 
-func (e *ConcurrentEngine) Run(seeds ...Request)  {
-	//吧请求提交到request chan中
-	for _,r:=range seeds{
-		e.Scheduler.Submit(r)
+func (e *ConcurrentEngine) Run(seeds ...Request) {
+
+	for _, r := range seeds {
+		if isDuplicate(r.Url) {
+			continue
+		}
+		e.Scheduler.Submit(r) //scheduler把请求提交到request chan中
 	}
 	//in:=make(chan Request)
-	out:=make(chan ParseResult)
+	out := make(chan ParseResult)
 	//e.Scheduler.ConfigureMasterWorkerChan(in)
 	//分发任务
 	e.Scheduler.Run()
@@ -29,34 +48,45 @@ func (e *ConcurrentEngine) Run(seeds ...Request)  {
 	//	createWorker(in,out)
 	//}
 	//创建多个work
-	for i:=0;i<e.WorkerCount;i++{
-		createWorker(out,e.Scheduler)
+	for i := 0; i < e.WorkerCount; i++ {
+		e.createWorker(e.Scheduler.WorkChan(), out, e.Scheduler)
 	}
 	for {
-		result:=<-out
-		for _,item:=range result.Items{
-			log.Printf("Got item: %v",item)
+		result := <-out
+		for _, item := range result.Items {
+			//log.Printf("Got item: %v",item)
+			go func() { e.ItemChan <- item }()
 		}
-		for _,request:=range result.Requests{
+		for _, request := range result.Requests {
+			if isDuplicate(request.Url) {
+				continue
+			}
 			e.Scheduler.Submit(request)
 		}
 	}
 }
+
 /**
 每个work开启一个goroutine
- */
-func createWorker(out chan ParseResult,s Scheduler)  {
-	in:=make(chan Request)
+*/
+//func createWorker(out chan ParseResult,s Scheduler)  {
+func (e *ConcurrentEngine) createWorker(
+	in chan Request,
+	out chan ParseResult, ready ReadyNotifier) {
+	//in := make(chan Request)
 	go func() {
-		for{
-			//每个work 都有一个channel
-			s.WorkerReady(in)
-			request:=<-in
-			result,err:=worker(request)
-			if err!=nil{
+		for {
+
+			ready.WorkerReady(in) //告诉scheduler,worker准备好了
+			request := <-in
+			// 换成call rpc, 这里是分布式的关键
+			result, e := e.RequestProcessor(request)
+			//result, e := Worker(request)
+			if e != nil {
 				continue
 			}
-			out<-result
+			out <- result
 		}
 	}()
+
 }
--- src/DistributedCrawler/engine/engine.go
+++ src/DistributedCrawler/engine/engine.go
@@ -4,21 +4,21 @@ import (
 	"log"
 )
 
-func Run(seeds ...Request)  {
-	var requests[] Request
-	for _,r:=range seeds{
-		requests=append(requests,r)
+func (e SimpleEngine) Run(seeds ...Request) {
+	var requests []Request
+	for _, r := range seeds {
+		requests = append(requests, r)
 	}
-	for len(requests)>0 {
-		r:=requests[0]
-		requests=requests[1:]
-		parseResult,err:=worker(r)
-		if err!=nil {
+	for len(requests) > 0 {
+		r := requests[0]
+		requests = requests[1:]
+		parseResult, err := worker(r)
+		if err != nil {
 			continue
 		}
-		requests=append(requests,parseResult.Requests...)
-		for _,item:=range parseResult.Items{
-			log.Printf("Got item %v",item)
+		requests = append(requests, parseResult.Requests...)
+		for _, item := range parseResult.Items {
+			log.Printf("Got item %v", item)
 		}
 	}
 }
