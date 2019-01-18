76335a780a0aba1836ca9b88a42b674ee4a39378
cst5201 <1015485519@qq.com>
2019-01-18 23:39:06 +0800

    优化调度器，增加注释

--- src/DistributedCrawler/scheduler/queued.go
+++ src/DistributedCrawler/scheduler/queued.go
@@ -4,39 +4,53 @@ import (
 	"DistributedCrawler/engine"
 )
 
-type QueuedScheduler struct{
+//队列调度器
+type QueuedScheduler struct {
 	requestChan chan engine.Request
-	workerChan chan chan engine.Request
+	workerChan  chan chan engine.Request
 }
-func (s *QueuedScheduler)Submit(r  engine.Request)  {
-	s.requestChan<-r
+
+func (s *QueuedScheduler) WorkerChan() chan engine.Request {
+	return make(chan engine.Request)
 }
-func (s *QueuedScheduler) WorkerReady(w chan engine.Request){
-	s.workerChan<-w
+
+//提交任务
+func (s *QueuedScheduler) Submit(r engine.Request) {
+	s.requestChan <- r
 }
-func (s *QueuedScheduler)ConfigureMasterWorkerChan(c chan engine.Request)  {
+func (s *QueuedScheduler) WorkerReady(w chan engine.Request) {
+	s.workerChan <- w
 }
-func (s *QueuedScheduler) Run(){
+
+//func (s *QueuedScheduler)ConfigureMasterWorkerChan(c chan engine.Request)  {
+//}
+func (s *QueuedScheduler) Run() {
+	s.workerChan = make(chan chan engine.Request)
+	s.requestChan = make(chan engine.Request)
+
 	go func() {
-		s.workerChan=make(chan  chan engine.Request)
-		s.requestChan=make(chan engine.Request)
-		var requestQ [] engine.Request
-		var workerQ [] chan engine.Request
+		//建立Request队列和worker队列
+		var requestQ []engine.Request
+		var workerQ []chan engine.Request
 		for {
+			//查看request和worker是否同时存在，若有则取出
 			var activeRequest engine.Request
 			var activeWorker chan engine.Request
-			if len(requestQ)>0 && len(workerQ)>0{
-				activeRequest=requestQ[0]
-				activeWorker=workerQ[0]
+			if len(requestQ) > 0 && len(workerQ) > 0 {
+				activeRequest = requestQ[0]
+				activeWorker = workerQ[0]
 			}
 			select {
+			//调度器中有请求时，将请求加入到请求队列
 			case r := <-s.requestChan:
 				requestQ = append(requestQ, r)
+			//调度器中有可以接收任务的worker时，将请求加入到worker中
 			case w := <-s.workerChan:
 				workerQ = append(workerQ, w)
-			case activeWorker<-activeRequest:
-				workerQ=workerQ[1:]
-				requestQ=requestQ[1:]
+			//当同时有请求又有worker时，将请求分配给worker执行，并从队列中移除
+			case activeWorker <- activeRequest:
+				workerQ = workerQ[1:]
+				requestQ = requestQ[1:]
 			}
 		}
 	}()
--- src/DistributedCrawler/scheduler/simple.go
+++ src/DistributedCrawler/scheduler/simple.go
@@ -6,11 +6,23 @@ type SimpleScheduler struct {
 	workerChan chan engine.Request
 }
 
-func (s *SimpleScheduler)Submit(r  engine.Request)  {
+func (s *SimpleScheduler) WorkerChan() chan engine.Request {
+	return s.workerChan
+}
+
+func (s *SimpleScheduler) WorkerReady(chan engine.Request) {
+	//panic()
+}
+
+func (s *SimpleScheduler) Run() {
+	s.workerChan = make(chan engine.Request)
+}
+func (s *SimpleScheduler) Submit(r engine.Request) {
 	go func() {
-		s.workerChan<-r
+		s.workerChan <- r
 	}()
 }
-func (s *SimpleScheduler)ConfigureMasterWorkerChan(c chan engine.Request)  {
-	s.workerChan=c
-}
+
+//func (s *SimpleScheduler)ConfigureMasterWorkerChan(c chan engine.Request)  {
+//	s.workerChan=c
+//}
